ddc82836650b6fcc9841d556c9e85c32a4cd0eaf
Tanush Mathur <devspark2501@gmail.com>
2026-09-01 15:00:27 +0530

    folders created and journal and main.dart is updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fitflow/Screens/%20home/home_page.dart';
 import 'package:flutter/material.dart';
+// import 'package:fitflow/screens/home/home_page.dart';
 
 void main() {
   runApp(FitFlowCode());
@@ -12,7 +13,22 @@ class FitFlowCode extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFF5F9FF),
+
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFF2196F3),
+        ),
+
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Color(0xFF1565C0),
+          elevation: 0,
+        ),
+      ),
+
+      home: HomePage()
     );
   }
 }
