3050934341e28679739f6a2d2a3e4dbd151d015a
Tanush Mathur <devspark2501@gmail.com>
2026-09-01 15:09:06 +0530

    home_page.dart updated

--- lib/screens/ home/home_page.dart
+++ lib/screens/ home/home_page.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
 import '../../widgets/app_drawer.dart';
 
+// import '../../components/home/welcome_section.dart';
+// import '../../components/home/daily_workout_card.dart';
+// import '../../components/home/quick_actions.dart';
+// import '../../components/home/progress_card.dart';
+
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text(
+          "FitFlow",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
 
       drawer: AppDrawer(),
 
-      body: Center(
-        child: Text("Home Page"),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+
+            //WelcomeSection(),
+
+            SizedBox(height: 20),
+
+            //DailyWorkoutCard(),
+
+            SizedBox(height: 20),
+
+            //QuickActions(),
+
+            SizedBox(height: 20),
+
+            //ProgressCard(),
+          ],
+        ),
       ),
     );
   }
